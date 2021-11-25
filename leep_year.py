993ddf2e40c0648dda1a202ad9da54535c47546a
kabita kumari <74282784+kabitakumari20@users.noreply.github.com>
2021-11-25 15:59:39 +0530

    Add files via upload

---
+++ leep_year.py
@@ -0,0 +1,15 @@
+# year=int(input("Enter the year:-"))
+# if year%4==0:
+#     if year%400==0:
+#         print("leep year")
+#     else:
+#         print("not leep year")
+# else:
+#     print("66666666666666")
+
+
+year=int(input("Enter the year:-"))
+if year%4==0 and year%400==0:
+    print("leep year")
+else:
+    print("not leep year")
